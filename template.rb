44ab7db872babd176eec421a8f147ee6d844271d
griffin <g@gmail.com>
2025-03-30 23:46:09 +0900

    Initial commit: Add template CLI tool

---
+++ template.rb
@@ -0,0 +1,20 @@
+class Template < Formula
+  # GitHub repository information
+  GITHUB_USER = "griffin928"
+  GITHUB_REPO = "homebrew-template"
+  VERSION = "v1.0.0"
+
+  desc "My personal CLI tool for daily automation"
+  homepage "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}"
+  url "#{homepage}/archive/refs/tags/#{VERSION}.tar.gz"
+  sha256 "<填入正确的 SHA256>"
+  license "MIT"
+
+  def install
+    bin.install "template"
+  end
+
+  test do
+    system "#{bin}/template"
+  end
+end
