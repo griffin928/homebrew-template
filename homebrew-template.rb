4886f871f4260fde77eaf1224d158d377d75c365
griffin <g@gmail.com>
2025-03-31 02:31:09 +0900

    chore: Update SHA256 for version v1.0.4

--- homebrew-template.rb
+++ homebrew-template.rb
@@ -7,7 +7,7 @@ class HomebrewTemplate < Formula
   desc "My personal CLI tool for daily automation"
   homepage "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}"
   url "#{homepage}/archive/refs/tags/#{VERSION}.tar.gz"
-  sha256 "b4913617758b32f4b754bff62ecd17ce786ea57b5ccdae62c396ce580d8852ef"
+  sha256 "8fc53359b8173a4c078b4acb0028fb45fab37679450e35c93270103a88cfdbbf"
   license "MIT"
 
   def install
