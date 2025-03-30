b51814ad9dbaee21a2d5374ef114c6e129b6580c
griffin <g@gmail.com>
2025-03-31 02:28:11 +0900

    chore: Bump version to v1.0.3

--- homebrew-template.rb
+++ homebrew-template.rb
@@ -2,12 +2,12 @@ class HomebrewTemplate < Formula
   # GitHub repository information
   GITHUB_USER = "griffin928"
   GITHUB_REPO = "homebrew-template"
-  VERSION = "v1.0.2"
+  VERSION = "v1.0.3"
 
   desc "My personal CLI tool for daily automation"
   homepage "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}"
   url "#{homepage}/archive/refs/tags/#{VERSION}.tar.gz"
-  sha256 "25298064db55daf48f39a8dfc85e69360ec9635bbfda855eac07acdd6a0e6818"
+  sha256 "b4913617758b32f4b754bff62ecd17ce786ea57b5ccdae62c396ce580d8852ef"
   license "MIT"
 
   def install
