f63458a8036bbe33e4bd3c0e29c58a6c1506756a
griffin <g@gmail.com>
2025-03-31 10:16:30 +0900

    更新版本号到 v1.0.15 并更新 SHA256

--- homebrew-template.rb
+++ homebrew-template.rb
@@ -2,12 +2,12 @@ class HomebrewTemplate < Formula
   # GitHub repository information
   GITHUB_USER = "griffin928"
   GITHUB_REPO = "homebrew-template"
-  VERSION = "v1.0.11"
+  VERSION = "v1.0.15"
 
   desc "My personal CLI tool for daily automation"
   homepage "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}"
   url "#{homepage}/archive/refs/tags/#{VERSION}.tar.gz"
-  sha256 "7d68a7bad5054e6a77fd30837d5076b10854a4c19845525282b051506438437d"
+  sha256 "5dc58a200839551010dba10f10fd5f468c9a1a3eab89a92345501175dbd987c7"
   license "MIT"
 
   def install
