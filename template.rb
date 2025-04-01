9118d5b770df789f17794de626104008e8eb350e
griffin <g@gmail.com>
2025-04-02 00:14:13 +0900

    发布版本 v1.0.57 并更新 SHA256

--- template.rb
+++ template.rb
@@ -2,12 +2,12 @@ class Template < Formula
   # GitHub repository information
   GITHUB_USER = "griffin928"
   GITHUB_REPO = "homebrew-template"
-  VERSION = "v1.0.53"
+  VERSION = "v1.0.57"
 
   desc "My personal CLI tool for daily automation"
   homepage "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}"
   url "#{homepage}/archive/refs/tags/#{VERSION}.tar.gz"
-  sha256 "c0bf3b4746627bd6ec9ff30ad3f39ebb64cc7499d63dc2f6905da3c33b442162"
+  sha256 "ae154f35a3ef041897d5633d47401fdead5ad8a9851c4aee55ac01a3758de6fe"
   license "MIT"
 
   def install
